04da04725a20cccd0836b890f4456c16c6f95dc2
Ihsan <ihsan@adroady.com>
2020-02-01 11:07:37 +0700

    firstcommit

---
+++ chaceFunction.js
@@ -0,0 +1,5 @@
+function chaceAdd(num1, num2) {
+  let add = num1 + num2;
+  console.log(add);
+}
+chaceAdd(2, 4);
---
+++ reverse.js
@@ -0,0 +1,8 @@
+const firstLetter = "shopee";
+
+function reverse(params) {
+  let toArr = params.split(",");
+  console.log(toArr);
+}
+
+reverse(firstLetter);
